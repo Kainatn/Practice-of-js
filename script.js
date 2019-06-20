628473f4194857a0ced96e8da9c653a4f78148dc
Kainat Naz <49521321+Kainatn@users.noreply.github.com>
2019-06-20 17:55:58 +0500

    added_files

---
+++ script.js
@@ -0,0 +1,26 @@
+const Shahrah_speed =60 ;
+let car_Speed=70;
+
+if(car_Speed > Shahrah_speed){
+ 
+        console.log("Over speed");
+    }
+else{
+            
+    console.log("Not");
+}
+
+let user="Uzma";
+if(user == "Uzma"){
+    console.log('WElcome');
+}
+else{
+    console.log('Not uzma');
+}
+let money=1000000;
+if(money >= 1000000 ){
+    console.log('WElcome bank');
+}
+else{
+    console.log('get out');
+}
