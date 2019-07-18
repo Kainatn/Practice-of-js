2b3ed94f0d8a3cf4f8bd330f824de2ad6b9c9dea
Kainat Naz <49521321+Kainatn@users.noreply.github.com>
2019-07-18 17:54:02 +0500

    Add files via upload

---
+++ function/script.js
@@ -0,0 +1,36 @@
+//functions
+function addNumber(numb1, numb2, numb3) {
+    return numb1 + numb2 + numb3
+}
+function paisaDouble(paisa) {
+    return paisa * 2;
+}
+function sayEidMurak(getName) {
+    return "Eid Mubarak  " + getName;
+
+}
+function giveLength(letter) {
+    return letter.length;
+
+}
+function makeItCApital(letter) {
+    return letter.toUpperCase();
+
+}
+function splitIt(letter) {
+    return letter.split();
+}
+//Json
+let human = {
+    name:"Sara",
+    age:55,
+    isHappy:false,
+    address:{
+        street:"House no 1, Lane 5",
+        city:"KArchi",
+        province:"Sindh",
+        country:"Pakistan",
+    },
+    hobbies:["watching movies","Playin games"]
+
+}
